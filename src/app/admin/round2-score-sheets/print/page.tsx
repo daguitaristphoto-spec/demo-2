d57f32d5ce4ca02c89b3d9ece40d3e386edc0dc4
daguitaristphoto-spec <daguitarist.photo@gmail.com>
2026-04-29 19:32:27 +0700

    Update page.tsx

--- src/app/admin/round2-score-sheets/print/page.tsx
+++ src/app/admin/round2-score-sheets/print/page.tsx
@@ -262,20 +262,20 @@ export default async function Round2ScoreSheetsPrintPage() {
             </section>
 
             <section className="signature-section">
-              <div>
-                <strong>Chữ ký Thư ký</strong>
-                <br />
-                <br />
-                <br />
-              </div>
+  <div>
+    <strong>THƯ KÝ</strong>
+    <br />
+    <br />
+    <br />
+  </div>
 
-              <div>
-                <strong>Chữ ký Ban Giám khảo</strong>
-                <br />
-                <br />
-                <br />
-              </div>
-            </section>
+  <div>
+    <strong>GIÁM KHẢO</strong>
+    <br />
+    <br />
+    <br />
+  </div>
+</section>
 
             <div className="sheet-footer">
               Phiếu {index + 1} / {sheetRows.length}
